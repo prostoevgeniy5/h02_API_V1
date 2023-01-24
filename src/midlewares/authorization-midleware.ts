f0b3cd63635f71cee619055e53427f15ee3c3650
evgeniy <yevgeniy5@ukr.net>
2023-01-25 00:37:56 +0200

    debug authorization-midleware.ts

--- src/midlewares/authorization-midleware.ts
+++ src/midlewares/authorization-midleware.ts
@@ -12,8 +12,10 @@ export const authMidleware =  (req: Request, res: Response, next: NextFunction) 
    if((req.method === "POST" || req.method === "PUT" || req.method === "DELETE") && req.headers.authorization) {
     //  let parole = "Basic YWRtaW46cXdlcnR5"
      let loginPasswordEncoded = btoa("admin:qwerty")
-    //  console.log(req.headers.authorization.split(' ')[1], "===", loginPasswordEncoded)
-     if(req.headers.authorization.split(' ')[1] === loginPasswordEncoded) {
+     console.log(req.headers.authorization.split(' ')[0])
+    console.log(req.headers.authorization.split(' ')[1], "===", loginPasswordEncoded)
+    console.log(atob(req.headers.authorization.split(' ')[1]))
+     if(req.headers.authorization.split(' ')[0] === 'Basic' && req.headers.authorization.split(' ')[1] === loginPasswordEncoded) {
         next()
         return
      } else {
