edcb79682d4440c7a57ec597eb6a76e85cae454a
evgeniy <yevgeniy5@ukr.net>
2023-01-24 04:49:00 +0200

    fixing errors delete-router. Used async await

--- src/routes/delete-router.ts
+++ src/routes/delete-router.ts
@@ -4,12 +4,12 @@ import { db, Videos, PostsType, BloggersType, CoursesType } from "../repositorie
 
 export const deleteRouter = Router()
     
-deleteRouter.delete('/', (req, res) => {
+deleteRouter.delete('/', async (req, res) => {
     
-    db.videos.splice(0)
-    db.posts.splice(0)
-    db.bloggers.splice(0)
-    db.courses.splice(0)
+    await db.videos.splice(0)
+    await db.posts.splice(0)
+    await db.bloggers.splice(0)
+    await db.courses.splice(0)
     if(db.videos.length === 0 && db.posts.length === 0 && db.bloggers.length === 0 &&  db.courses.length === 0) {
       console.log('db.posts, db.bloggers, db.videos', db.posts)
       res.sendStatus(HTTP_STATUSES.NO_CONTENT_204)
