1cbf4ff3fd50a15d7b5d89ab145ef595f398553b
evgeniy <yevgeniy5@ukr.net>
2023-01-26 19:55:13 +0200

    has been fixed posts-router.ts file

--- src/routes/posts-router.ts
+++ src/routes/posts-router.ts
@@ -1,5 +1,4 @@
 import { Request, Response, Router } from 'express'
-// import {bloggers} from './bloggers-router'
 import { db, PostsType, BloggersType } from '../repositories/db'
 import { body,check, validationResult, CustomValidator } from 'express-validator'
 
@@ -15,8 +14,6 @@ interface errorsType {
   errorsMessages: errorsDescription[]
 }
 
-
-
 function errorFields():errorsType {
   return {
     errorsMessages: []
@@ -26,13 +23,13 @@ function errorFields():errorsType {
 export const postsRouter = Router({})
 
 postsRouter.get('/', (req: Request, res: Response) => {
-  res.status(200).send(posts);
+  res.status(200).json(posts);
 });
 
-postsRouter.get('/:id', (req: Request, res: Response) => {
-  let postItem = posts.find(item => +item.id === +req.params.id);
+postsRouter.get('/:id', async (req: Request, res: Response) => {
+  let postItem = await posts.find(item => +item.id === +req.params.id);
   if (postItem) {
-    res.status(200).send(postItem);
+    res.status(200).json(postItem);
     return
   } else {
     res.sendStatus(404);
@@ -45,8 +42,49 @@ postsRouter.post('/',
   body('shortDescription').isString().withMessage('must be string').notEmpty().withMessage('must be not empty').isLength({ max: 100 }).withMessage('length must be less than 100 characters'),
   body('content').isString().withMessage('must be string').notEmpty().withMessage('must be not empty').isLength({ max: 1000 }).withMessage('length must be less than 1000 characters'),
   check('blogId').isString().withMessage('must be string').trim().notEmpty().withMessage('must be not empty').custom((value, { req: Request }) => {
-    console.log('value', value)
-    console.log('{req: Request}', {req: Request}.req.body)
+    let blogger: BloggersType | undefined = bloggers.find((item: BloggersType) => +item.id === +value)
+    const result =  blogger ? true : false;
+    return result
+  }).withMessage("A blogger with such a blogId does not exist"),
+  
+  (req: Request, res: Response) => {
+    const postRequestErrors: errorsType = errorFields();
+    const errors = validationResult(req)
+    console.log('errors', errors)
+    if (!errors.isEmpty()) {
+      errors.array().forEach((elem, ind) => {
+        const obj = { 
+          "message": elem.msg,
+          "field": elem.param}
+        postRequestErrors.errorsMessages.push(obj)
+      })
+      res.status(400).json(postRequestErrors)
+      return
+    }
+
+    let blogger = bloggers.find(item => +item.id === +req.body.blogId)
+    let newPost
+    if (blogger) {
+      newPost = {
+        id: (+(new Date())).toString(),
+        "title": req.body.title,
+        "shortDescription": req.body.shortDescription,
+        "content": req.body.content,
+        "blogId": req.body.blogId,
+        "blogName": blogger.name
+      };
+      db.posts.push(newPost);
+      res.status(201).json(newPost);
+      return
+    }
+
+});
+
+postsRouter.put('/:id',
+  body('title').isString().withMessage('must be string').trim().notEmpty().withMessage('must be not empty').isLength({ max: 30 }).withMessage('length must be less than 30 characters'),
+  body('shortDescription').isString().withMessage('must be string').trim().notEmpty().withMessage('must be not empty').isLength({ max: 100 }).withMessage('length must be less than 100 characters'),
+  body('content').isString().withMessage('must be string').trim().notEmpty().withMessage('must be not empty').isLength({ max: 1000 }).withMessage('length must be less than 1000 characters'),
+  check('blogId').isString().withMessage('must be string').trim().notEmpty().withMessage('must be not empty').custom((value, { req: Request }) => {
     let blogger: BloggersType | undefined = bloggers.find((item: BloggersType) => +item.id === +value)
     const result =  blogger ? true : false;
     return result
@@ -64,49 +102,14 @@ postsRouter.post('/',
     })
     return res.status(400).json(postRequestErrors)
   }
-
-  let blogger = bloggers.find(item => +item.id === +req.body.blogId)
-  let newPost
-  if (blogger) {
-    newPost = {
-      id: (+(new Date())).toString(),
-      "title": req.body.title,
-      "shortDescription": req.body.shortDescription,
-      "content": req.body.content,
-      "blogId": req.body.blogId,
-      "blogName": blogger.name
-    };
-    db.posts.push(newPost);
-    res.status(201).send(newPost);
-    return
-  }
-
-});
-
-postsRouter.put('/:id', (req: Request, res: Response) => {
   let index: number;
-  // const putRequestErrors = errorFields();
-
   let blogger = bloggers.find(item => +item.id === +req.body.blogId)
-
   let postItem = posts.find((item, ind) => {
     if (+item.id === +req.params.id) {
       index = ind;
     }
     return +item.id === +req.params.id;
   });
-
-  const postRequestErrors: errorsType = errorFields();
-  const errors = validationResult(req)
-  if (!errors.isEmpty()) {
-    errors.array().forEach((elem, ind) => {
-      const obj = { 
-        "message": elem.msg,
-        "field": elem.param}
-      postRequestErrors.errorsMessages.push(obj)
-    })
-    return res.status(400).json(postRequestErrors)
-  }
 
   if (postItem && blogger) {
     posts = posts.map((item, i) => {
@@ -121,7 +124,7 @@ postsRouter.put('/:id', (req: Request, res: Response) => {
       }
       return item;
     });
-    res.sendStatus(204).send('No Content');
+    res.status(204).send('No Content');
   }
   else {
     res.sendStatus(404);
@@ -134,12 +137,14 @@ postsRouter.delete('/:id', (req: Request, res: Response) => {
     return +item.id !== Number.parseInt(req.params.id);
   });
   if (length > posts.length) {
-    res.send(204);
+    res.sendStatus(204);
+    return
   } else {
-    res.send(404);
+    res.sendStatus(404);
+    return
   }
 });
-function res(error?: any): void {
-  throw new Error('Function not implemented.')
-}
+// function res(error?: any): void {
+//   throw new Error('Function not implemented.')
+// }
 
