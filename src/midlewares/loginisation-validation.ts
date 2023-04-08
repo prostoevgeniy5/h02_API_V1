3b4ddcf9ba9c3c08382a25d484f4aabd9eb42d1f
evgeniy <yevgeniy5@ukr.net>
2023-04-08 17:32:47 +0300

    was fixed loginisation-validation.ts file (.trim())

--- src/midlewares/loginisation-validation.ts
+++ src/midlewares/loginisation-validation.ts
@@ -2,6 +2,6 @@ import { body } from "express-validator"
 
 export const loginOrEmailPasswordValidation = [
   // const pattern = new RegExp("^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$")
-  body('loginOrEmail').exists().withMessage('The loginOrEmail field not exist').isString().withMessage('The loginOrEmail field is not string').trim().notEmpty().withMessage('The loginOrEmail field is empty'),
-  body('password').exists().withMessage('The password field not exist').isString().withMessage('The password field is not string').trim().notEmpty().withMessage('The password field is empty')
+  body('loginOrEmail').exists().withMessage('The loginOrEmail field not exist').trim().isString().withMessage('The loginOrEmail field is not string').notEmpty().withMessage('The loginOrEmail field is empty'),
+  body('password').exists().withMessage('The password field not exist').trim().isString().withMessage('The password field is not string').notEmpty().withMessage('The password field is empty')
 ]
