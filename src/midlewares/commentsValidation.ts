3aa691f0a4cd277ac2bad00d4f4ccccc0f65cbec
evgeniy <yevgeniy5@ukr.net>
2023-04-09 22:31:58 +0300

    fixed commentsValidation function midlewares/commentsValidation.ts

--- src/midlewares/commentsValidation.ts
+++ src/midlewares/commentsValidation.ts
@@ -1,5 +1,3 @@
 import { body } from 'express-validator'
 
-export const commentsValidation = (req: Request) => {
-  body('content').isString().trim().notEmpty().isLength({min: 20, max: 300}).withMessage('field content is not valide')
-}
+export const commentsValidation = body('content').isString().trim().notEmpty().isLength({min: 20, max: 300}).withMessage('field content is not valide')
