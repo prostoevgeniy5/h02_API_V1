cbafb590cad1b513f4f73f4bee8d8ca131eefd33
evgeniy <yevgeniy5@ukr.net>
2023-05-22 15:00:38 +0300

    was to install cookie-parser to project

--- src/repositories/settings.ts
+++ src/repositories/settings.ts
@@ -1,5 +1,6 @@
 export const settings = {
   JWT_SECRET: process.env.JWT_SECRET || '123456',
+  REFRESH_JWT_SECRET: process.env.REFRESH_JWT_SECRET || '1234567',
   GMAIL: process.env.GMAIL,
   GMAIL_PASSWORD: process.env.GMAIL_PASS,
   EMAIL: process.env.EMAIL,
